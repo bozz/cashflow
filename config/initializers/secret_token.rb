0f764ce9a111b65d6ce0e5508556cb88a954c925
Boris Searles <boris@lucidgardens.com>
2011-04-22 22:43:19 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cashflow::Application.config.secret_token = '112f0f41c96affed4f3ec3aa02d5c7bf45e978204d2c442463df75b9e695a0687175d8b76347da4eeea2d45aa2bfc5535a8810f62fc1350d42c2d9a3333e8718'
