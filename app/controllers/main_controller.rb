4be3e95f191a6c39c8b8de650fb975e35296b538
Boris Searles <boris@lucidgardens.com>
2012-06-12 15:00:10 +0200

    change ordering of transactions to descending

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,6 @@ class MainController < ApplicationController
     # bootstrap app with the following data
     @accounts = Account.order('identifier')
     @bank_accounts = BankAccount.all
-    @transactions = BankTransaction.order('date')
+    @transactions = BankTransaction.order('date DESC')
   end
 end
